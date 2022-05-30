28f88da325f0f145e584128c5ebeedb4228ed862
Salman Asif S <37960662+Salman-Asif@users.noreply.github.com>
2022-05-31 01:37:08 +0530

    Time: 6 ms (59.13%), Space: 108.4 MB (15.75%) - LeetHub

--- 643-maximum-average-subarray-i/643-maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/643-maximum-average-subarray-i.java
@@ -1,10 +1,10 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         
-        int windowStart = 0, windowSum=0;
+        int windowStart = 0, windowSum=0, length = nums.length;
         double averageSum = -Double.MAX_VALUE, maxAverage = -Double.MAX_VALUE;
  
-        for(int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+        for(int windowEnd = 0; windowEnd < length; windowEnd++) {
             windowSum = windowSum + nums[windowEnd];
             if(windowEnd >= k-1) {
                 averageSum = (double)windowSum/k;
