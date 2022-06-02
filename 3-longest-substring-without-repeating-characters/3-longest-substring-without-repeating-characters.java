1be35db6dbd4ddc4bad9d1c36266cf059e26c76f
Salman Asif S <37960662+Salman-Asif@users.noreply.github.com>
2022-06-03 00:46:19 +0530

    Time: 11 ms (56.78%), Space: 45 MB (45.73%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -6,19 +6,12 @@ class Solution {
         
         for(int windowEnd = 0; windowEnd < length; windowEnd++) {
             ch = s.charAt(windowEnd);
-          
-            charMap.put(ch, charMap.getOrDefault(ch,0)+1);
             
-               
-                while(charMap.get(ch) > 1) {
-                    charLeft = s.charAt(windowStart);
-                    charMap.put(charLeft, charMap.get(charLeft)-1);
-                    if(charMap.get(charLeft) == 0) {
-                        charMap.remove(charLeft);
-                    }
-                    windowStart++;
-                }
+            if(charMap.containsKey(ch) ) {
+                windowStart = Math.max(windowStart, charMap.get(ch)+1);
+            }            
             
+            charMap.put(ch, windowEnd);
             maxLength = Math.max(maxLength, windowEnd-windowStart+1);
         }
         return maxLength;
