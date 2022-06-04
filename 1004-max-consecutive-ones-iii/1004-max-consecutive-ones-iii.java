b3d0d201aab9e00acbe707c76817e7a13b88e1c6
Salman Asif S <37960662+Salman-Asif@users.noreply.github.com>
2022-06-05 00:56:55 +0530

    Time: 6 ms (15.93%), Space: 71.1 MB (38.54%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,24 +1,17 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int windowStart=0, maxOnesAfterReplacement = 0, maxOnes=0, length = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        int currentElement=-1;
+        int ones=0;
         
         for(int windowEnd =0; windowEnd < length ; windowEnd++) {
             
-            currentElement = nums[windowEnd];
-            map.put(currentElement, map.getOrDefault(currentElement,0)+1);
-            
-            if(currentElement == 1) {
-                maxOnes = Math.max(maxOnes, map.get(currentElement));
+            if(nums[windowEnd] == 1) {
+                ones++;
             }
             
-            if(windowEnd - windowStart +1 - maxOnes > k) {
-                int leftElement = nums[windowStart];
-                map.put(leftElement, map.get(leftElement)-1);
-                if(map.get(leftElement) == 0) {
-                    map.remove(leftElement);
-                }
+            while(windowEnd - windowStart +1 - ones > k) {
+                if(nums[windowStart] == 1)
+                    ones--;
                 windowStart++;
             }
             
